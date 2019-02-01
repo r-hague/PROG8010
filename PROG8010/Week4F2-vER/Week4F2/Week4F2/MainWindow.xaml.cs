6b5976d354c0ed6070f130894130aa8869caf284
r-hague <r.m.hague@gmail.com>
2019-02-01 13:09:41 -0500

    Add files via upload

---
+++ PROG8010/Week4F2-vER/Week4F2/Week4F2/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Week4F2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        VM vm = new VM();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = vm;
+
+            vm.CurrentIndex = 2;
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (LbItems.SelectedItem != null)
+            {
+                string tag = (string)((ListBoxItem)LbItems.SelectedItem).Tag;
+
+                string word = CbEnglish.IsChecked == true ? "One" : "Un";
+
+                switch (tag)
+                {
+                    case "0":
+                        LblOut.Content = $"{word} Million Pounds";
+                        break;
+                    case "1":
+                        LblOut.Content = $"{word} Billion Pounds";
+                        break;
+                    case "2":
+                        LblOut.Content = $"{word} Trillion Pounds";
+                        break;
+                    default:
+                        LblOut.Content = $"{word} Pound";
+                        break;
+                }
+            }
+        }
+
+        private void CbEnglish_Checked(object sender, RoutedEventArgs e)
+        {
+            ListBox_SelectionChanged(null, null);
+        }
+    }
+}
---
+++ PROG8010/Week4F2-vER/Week4F2/Week4F2/VM.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4F2
+{
+    class VM : INotifyPropertyChanged
+    {
+        private int currentIndex = -1;
+        public int CurrentIndex
+        {
+            get { return currentIndex; }
+            set { currentIndex = value; calcText(); NotifyChanged(); }
+        }
+
+        private bool english = false;
+        public bool English
+        {
+            get { return english; }
+            set { english = value; calcText(); NotifyChanged(); }
+        }
+
+        private string displayText = "";
+        public string DisplayText
+        {
+            get { return displayText; }
+            set { displayText = value; NotifyChanged(); }
+        }
+
+        private string inputText = "";
+        public string InputText
+        {
+            get { return inputText; }
+            set { inputText = value; calcText(); NotifyChanged(); }
+        }
+
+        private void calcText()
+        {
+            if (CurrentIndex >= 0)
+            {
+                string word = English == true ? "One" : "Un";
+
+                switch (CurrentIndex)
+                {
+                    case 0:
+                        DisplayText = $"{inputText} {word} Million Pounds";
+                        break;
+                    case 1:
+                        DisplayText = $"{inputText} {word} Billion Pounds";
+                        break;
+                    case 2:
+                        DisplayText = $"{inputText} {word} Trillion Pounds";
+                        break;
+                    default:
+                        DisplayText = $"{inputText} {word} Pound";
+                        break;
+                }
+            }
+        }
+
+        #region PropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        #endregion
+    }
+}
